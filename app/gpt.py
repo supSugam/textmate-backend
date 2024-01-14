5113dd266d4538dd2c333bf91dd39df02c3dbbc0
supSugam <sugamsubedi234@gmail.com>
2024-01-14 21:17:50 +0545

    changes

--- app/gpt.py
+++ app/gpt.py
@@ -5,29 +5,26 @@ import os
 import re
 
 client = OpenAI(
-    # api_key=os.getenv("OPENAI_API_KEY"),
-    api_key="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",  # this is also the default, it can be omitted
+    api_key=os.getenv("OPENAI_API_KEY"),
 )
 
-def generate_response(file_content, user_query=None):
+def generate_summary(file_content):
+            # Step 1: Preprocess the text
+    summarized_text = preprocess_text(file_content)
     try:
-        # Step 1: Preprocess the text
-        summarized_text = preprocess_text(file_content)
-
         # Step 2: Perform sentiment analysis
         sentiment_result = perform_sentiment_analysis(summarized_text)
 
         # Step 3: Generate response using GPT-3.5 based on user query and combined features
-        gpt_prompt = f"File Summary: {summarized_text}"
-        if user_query:
-            gpt_prompt += f"\n\nUser Query: {user_query}\n\nSentiment: {sentiment_result}\n\nNow, provide a response:"
+        gpt_prompt = f"File Summary: {summarized_text} \nSentiment: {sentiment_result}, \nNow Answer based on the information provided, in readable sentences."
 
         response = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[{
                 "role": "user",
                 "content": gpt_prompt
-            }],
+            }
+            ],
             temperature=0.9,
             max_tokens=100,
             top_p=1,
@@ -43,4 +40,37 @@ def generate_response(file_content, user_query=None):
         return generated_response, sentences
 
     except Exception as e:
-        raise RuntimeError(f"Error generating response: {str(e)}")
+        return summarized_text, [summarized_text]
+    
+
+def respond_user_query(file_content, question):
+    # Step 1: Preprocess the text
+    summarized_text = preprocess_text(file_content)
+    try:
+        # Step 2: Perform sentiment analysis
+        sentiment_result = perform_sentiment_analysis(summarized_text)
+
+        # Step 3: Generate response using GPT-3.5 based on user query and combined features
+        gpt_prompt = f"Content: {summarized_text}\nSentiment: {sentiment_result}\nQuestion: {question}\n Now Answer based on the information provided, nothing extra."
+
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{
+                "role": "user",
+                "content": gpt_prompt
+            },
+            ],
+            temperature=0.9,
+            max_tokens=100,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0.6,
+            stop=["\n"]
+        )
+
+        generated_response = response.choices[0].message.content
+        # Return a tuple containing both the generated response and the summarized text
+        return generated_response
+
+    except Exception as e:
+        raise e
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, request, jsonify
 from flask_cors import cross_origin  # Import cross_origin
-from .gpt import generate_response
+from .gpt import generate_summary
+from .gpt import respond_user_query
 
 from app import app
 
@@ -9,18 +10,38 @@ from app import app
 def index():
     return jsonify(message='Welcome to TextMate!')
 
-@app.route('/process', methods=['POST'])
+@app.route('/summarize', methods=['POST'])
 @cross_origin()  # Enable CORS for this route
-def process_file():
+def summarize_file_contents():
     try:
         data = request.get_json()
         file_content = data.get('file_content', '')
 
         # Call the function to generate a response using GPT-3.5
-        summarized_text, sentences = generate_response(file_content)
+        summarized_text, sentences = generate_summary(file_content)
         data = {
         'summarized_text': summarized_text,
         'sentences': sentences,
+        }
+
+        return jsonify(data, 200, {'Content-Type': 'application/json; charset=utf-8'})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+
+@app.route('/ask', methods=['POST'])
+@cross_origin()  # Enable CORS for this route
+def ask_question():
+    try:
+        data = request.get_json()
+        file_content = data.get('file_content', '')
+        question = data.get('question', '')
+
+        # Call the function to generate a response using GPT-3.5
+        answer = respond_user_query(file_content, question)
+        data = {
+        'answer': answer,
         }
 
         return jsonify(data, 200, {'Content-Type': 'application/json; charset=utf-8'})
